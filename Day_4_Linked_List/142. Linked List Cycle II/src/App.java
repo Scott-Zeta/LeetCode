8a8215adfab97ab6cac271884c0246c156250571
Scott-Zeta <wc070211@gmail.com>
2022-09-22 09:52:28 +0930

    add chasing method with O(1) storage

--- Day_4_Linked_List/142. Linked List Cycle II/src/App.java
+++ Day_4_Linked_List/142. Linked List Cycle II/src/App.java
@@ -24,6 +24,40 @@ public class App {
         return null;
     }
 
+    public ListNode chasing_detectCycle(ListNode head){
+        if(head == null){
+            return null;
+        }
+
+        ListNode intersect = find_intersect(head);
+
+        if(intersect == null){
+            return null;
+        }
+
+        ListNode tracker = head;
+        while(!tracker.equals(intersect)){
+            tracker = tracker.next;
+            intersect = intersect.next;
+        }
+
+        return tracker;
+    }
+
+    public ListNode find_intersect(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow.equals(fast)){
+                return slow;
+            }
+        }
+        return null;
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
