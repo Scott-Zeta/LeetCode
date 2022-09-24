f10025c9e29f1a3c60079ea7c76fc6f1c7e44813
Scott-Zeta <wc070211@gmail.com>
2022-09-24 16:56:46 +0930

    little change

--- Day_3_Conditional_Statements/976. Largest Perimeter Triangle/src/App.java
+++ Day_3_Conditional_Statements/976. Largest Perimeter Triangle/src/App.java
@@ -3,16 +3,16 @@ import java.util.Arrays;
 public class App {
     public static int largestPerimeter(int[] nums) {
         Arrays.sort(nums);
-        for (int i = nums.length - 3; i >= 0; i--) {
-            if (nums[i + 2] < (nums[i] + nums[i + 1])) {
-                return nums[i + 2] + nums[i] + nums[i + 1];
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i] < (nums[i - 1] + nums[i - 2])) {
+                return nums[i] + nums[i - 1] + nums[i - 2];
             }
         }
         return 0;
     }
 
     public static void main(String[] args) throws Exception {
-        int[] nums = {2,1,1};
+        int[] nums = { 2, 1, 2 };
         System.out.println(largestPerimeter(nums));
     }
 }
