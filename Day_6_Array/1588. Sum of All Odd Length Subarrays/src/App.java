f5a7439089a8d66055d3fdb19f3d28718fd5972b
Scott-Zeta <wc070211@gmail.com>
2022-10-01 10:11:15 +0930

    day6 ex1

--- Day_6_Array/1588. Sum of All Odd Length Subarrays/src/App.java
+++ Day_6_Array/1588. Sum of All Odd Length Subarrays/src/App.java
@@ -1,8 +1,14 @@
 public class App {
 
     public int sumOddLengthSubarrays(int[] arr) {
-        
+        int sum = 0;
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            sum += ((i + 1) * (n - i) + 1) / 2 * arr[i];
+        }
+        return sum;
     }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
