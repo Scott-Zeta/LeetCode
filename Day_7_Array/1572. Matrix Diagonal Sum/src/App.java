e1889581994838c158fffadf66e331c92d16aa87
Scott-Zeta <wc070211@gmail.com>
2022-10-01 19:34:30 +0930

    day7 ex1

---
+++ Day_7_Array/1572. Matrix Diagonal Sum/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i];
+            sum += mat[i][n - 1 - i];
+        }
+        if (n % 2 != 0) {
+            return sum - mat[n/2][n/2];
+        } else {
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
