3ebcca8ece5c23a28d439332e9bc9e02666c8e2c
Scott-Zeta <wc070211@gmail.com>
2022-10-05 14:37:35 +1030

    day9 ex3

--- Day_9_String/953. Verifying an Alien Dictionary/src/App.java
+++ Day_9_String/953. Verifying an Alien Dictionary/src/App.java
@@ -3,10 +3,12 @@ import java.util.HashMap;
 public class App {
     public boolean isAlienSorted(String[] words, String order) {
         HashMap<Character, Integer> orderMap = getMap(order);
-        for (int i = 0; i < words.length; i++) {
-            
+        for (int i = 0; i < words.length - 1; i++) {
+            if (compareWords(words[i], words[i + 1], orderMap) == false) {
+                return false;
+            }
         }
-        return false;
+        return true;
     }
 
     public HashMap<Character, Integer> getMap(String order) {
@@ -29,8 +31,14 @@ public class App {
         for (int i = 0; i < length; i++) {
             int c1 = orderMap.get(word1.charAt(i));
             int c2 = orderMap.get(word2.charAt(i));
+
+            System.out.println("c1 " + i + ":" + c1);
+            System.out.println("c2 " + i + ":" + c2);
             if (c1 > c2) {
                 return false;
+            }
+            if(c1 < c2){
+                return true;
             }
         }
         if (m > n) {
