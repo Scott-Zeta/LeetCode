491173da28efb725d54a1cf621e994789c506460
Scott-Zeta <wc070211@gmail.com>
2022-10-06 10:50:13 +1030

    day10 ex4

--- Day_10_LinkedList_and_Tree/404. Sum of Left Leaves/src/App.java
+++ Day_10_LinkedList_and_Tree/404. Sum of Left Leaves/src/App.java
@@ -1,4 +1,48 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class App {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return result(root, false);
+    }
+
+    public int result(TreeNode root, boolean isLeft) {
+        if (root.left == null && root.right == null) {
+            if (isLeft) {
+                return root.val;
+            } else {
+                return 0;
+            }
+        }
+        int total = 0;
+        if (root.left != null) {
+            total += result(root.left, true);
+        }
+        if (root.right != null) {
+            total += result(root.right, false);
+        }
+        return total;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
