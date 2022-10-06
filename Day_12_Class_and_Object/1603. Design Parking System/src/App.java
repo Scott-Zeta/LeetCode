0560eeef80c311057f22c283bb135a46de3b4f10
Scott-Zeta <wc070211@gmail.com>
2022-10-06 18:11:32 +1030

    day12 ex1

--- Day_12_Class_and_Object/1603. Design Parking System/src/App.java
+++ Day_12_Class_and_Object/1603. Design Parking System/src/App.java
@@ -1,3 +1,37 @@
+class ParkingSystem {
+    int big;
+    int medium;
+    int small;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public boolean addCar(int carType) {
+        if(carType == 1){
+            if(big - 1 >= 0){
+                big--;
+                return true;
+            }
+        }
+        if(carType == 2){
+            if(medium - 1 >= 0){
+                medium--;
+                return true;
+            }
+        }
+        if(carType == 3){
+            if(small - 1 >= 0){
+                small--;
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
