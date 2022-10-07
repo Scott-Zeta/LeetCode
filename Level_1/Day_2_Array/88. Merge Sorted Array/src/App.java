4061a42b9b16e4f278feef5e2d3900c86288741f
Scott-Zeta <wc070211@gmail.com>
2022-10-07 17:37:22 +1030

    add stack method, but easier to has error

--- Level_1/Day_2_Array/88. Merge Sorted Array/src/App.java
+++ Level_1/Day_2_Array/88. Merge Sorted Array/src/App.java
@@ -1,4 +1,5 @@
 import java.util.Queue;
+import java.util.Stack;
 import java.util.LinkedList;
 
 public class App {
@@ -13,7 +14,7 @@ public class App {
         int p1 = 0;
         int p2 = 0;
 
-        //since sometimes need to skip the copy array, queue can not work in here
+        // since sometimes need to skip the copy array, queue can not work in here
 
         // Compare elements from nums1Copy and nums2 and write the smallest to nums1.
         for (int p = 0; p < m + n; p++) {
@@ -27,23 +28,41 @@ public class App {
         }
     }
 
-    //three pointer method easier to understand from end to start, may also can use stack?
+    // three pointer method easier to understand from end to start, may also can use
+    // stack?
     public void ThreePointermerge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1;
         int j = n - 1;
         int length = m + n - 1;
-        
-        while(i >= 0 && j >= 0) {
-            if(nums1[i] > nums2[j]) {
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
                 nums1[length] = nums1[i--];
             } else {
                 nums1[length] = nums2[j--];
             }
             length--;
         }
-        
-        while(j >= 0 && length >= 0) {
+
+        while (j >= 0 && length >= 0) {
             nums1[length--] = nums2[j--];
+        }
+    }
+
+    public void stackMerge(int[] nums1, int m, int[] nums2, int n) {
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            s.push(nums2[i]);
+        }
+        int pointer = m - 1;
+        for (int i = m + n - 1; i >= 0; i--) {
+            if (!s.isEmpty()) {
+                if (pointer >= 0 && nums1[pointer] > s.peek()) {
+                    nums1[i] = nums1[pointer--];
+                } else {
+                    nums1[i] = s.pop();
+                }
+            }
         }
     }
 
