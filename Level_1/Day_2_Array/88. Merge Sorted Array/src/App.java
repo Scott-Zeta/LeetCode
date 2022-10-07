8cce7d7b195e7412a455037a596270c8f5a206d4
Scott-Zeta <wc070211@gmail.com>
2022-10-07 16:33:07 +1030

    another method

--- Level_1/Day_2_Array/88. Merge Sorted Array/src/App.java
+++ Level_1/Day_2_Array/88. Merge Sorted Array/src/App.java
@@ -27,6 +27,26 @@ public class App {
         }
     }
 
+    //three pointer method easier to understand from end to start, may also can use stack?
+    public void ThreePointermerge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int length = m + n - 1;
+        
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[length] = nums1[i--];
+            } else {
+                nums1[length] = nums2[j--];
+            }
+            length--;
+        }
+        
+        while(j >= 0 && length >= 0) {
+            nums1[length--] = nums2[j--];
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         int[] nums1 = { 1, 2, 3, 0, 0, 0 };
         int[] nums2 = { 2, 5, 6 };
