a13f38dc92a6e25727a7a9e4f74adf669c4f0eec
Scott-Zeta <wc070211@gmail.com>
2022-10-10 18:09:16 +1030

    day3 ex1

--- Level_1/Day_3_Array/350. Intersection of Two Arrays II/src/App.java
+++ Level_1/Day_3_Array/350. Intersection of Two Arrays II/src/App.java
@@ -1,16 +1,24 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class App {
     public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int[] result = {};
-        for(int i = 0; i < nums1.length; i++){
-            map.put(nums1[i], i);
+        if (nums2.length > nums1.length) {
+            intersect(nums2, nums1);
         }
-
-        for(int i = 0; i< nums2.length; i++){
-            
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
         }
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int n : nums2) {
+            int get = map.getOrDefault(n, 0);
+            if (get > 0) {
+                result.add(n);
+                map.put(n, get - 1);
+            }
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public static void main(String[] args) throws Exception {
