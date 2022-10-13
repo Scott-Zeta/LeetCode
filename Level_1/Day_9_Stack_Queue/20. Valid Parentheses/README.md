c669a4fe134468b666898362975c45bb03065c4a
Scott-Zeta <wc070211@gmail.com>
2022-10-13 16:40:33 +1030

    day9 ex1

---
+++ Level_1/Day_9_Stack_Queue/20. Valid Parentheses/README.md
@@ -0,0 +1,48 @@
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+
+
+
+## Getting Started
+
+Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+
+## Folder Structure
+
+The workspace contains two folders by default, where:
+
+- `src`: the folder to maintain sources
+- `lib`: the folder to maintain dependencies
+
+Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+
+> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
+
+## Dependency Management
+
+The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
---
+++ Level_1/Day_9_Stack_Queue/20. Valid Parentheses/src/App.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class App {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            if (isLeft(c1)) {
+                stack.add(c1);
+            }
+            if (!isLeft(c1)) {
+                if (!stack.isEmpty()) {
+                    char c2 = stack.peek();
+                    if (matchUp(c2, c1)) {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isLeft(char c) {
+        if (c == '(' || c == '{' || c == '[') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean matchUp(char c1, char c2) {
+        if (c1 == '(' && c2 == ')') {
+            return true;
+        }
+        if (c1 == '[' && c2 == ']') {
+            return true;
+        }
+        if (c1 == '{' && c2 == '}') {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(isValid("{[}]"));
+        System.out.println("Hello, World!");
+    }
+}
