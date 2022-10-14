96418aca9bf9ac45f9aca4803abf1e2877f0ff22
Scott-Zeta <wc070211@gmail.com>
2022-10-14 12:32:52 +1030

    better solution

--- Level_1/Day_12_Slingding_Window/438. Find All Anagrams in a String/src/App.java
+++ Level_1/Day_12_Slingding_Window/438. Find All Anagrams in a String/src/App.java
@@ -22,7 +22,40 @@ public class App {
         return result;
     }
 
-    
+    public List<Integer> BetterfindAnagrams(String s, String p) {
+        int ns = s.length(), np = p.length();
+        if (ns < np)
+            return new ArrayList<>();
+
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
+        // build reference array using string p
+        for (char ch : p.toCharArray()) {
+            pCount[ch - 'a']++;
+        }
+
+        List<Integer> output = new ArrayList<>();
+        // sliding window on the string s
+        for (int i = 0; i < ns; ++i) {
+            // add one more letter
+            // on the right side of the window
+            sCount[s.charAt(i) - 'a']++;
+            // remove one letter
+            // from the left side of the window
+            if (i >= np) {
+                sCount[s.charAt(i - np) - 'a']--;
+            }
+            // compare array in the sliding window
+            // with the reference array
+            if (Arrays.equals(pCount, sCount)) {
+                output.add(i - np + 1);
+            }
+        }
+
+        // only keep one iteration in sliding window
+        return output;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
