805b77164f33003f3c25a4289b7d5eeeac1703a5
Scott-Zeta <wc070211@gmail.com>
2022-10-17 20:03:41 +1030

    try to implement two stack method

--- Level_1/Day_14_Stack/394. Decode String/src/App.java
+++ Level_1/Day_14_Stack/394. Decode String/src/App.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 public class App {
-    public String decodeString(String s) {
-        //one stack method, push out decoding then push back
-        //to deal with nested brackets
+    public String SingleStackDecodeString(String s) {
+        // one stack method, push out decoding then push back
+        // to deal with nested brackets
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == ']') {
@@ -42,6 +42,41 @@ public class App {
         }
 
         return new String(result);
+    }
+
+    public String decodeString(String s) {
+        Stack<Integer> count = new Stack<>();
+        Stack<StringBuilder> content = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        int k = 0;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                // record k repeat number
+                k = k * 10 + (c - '0');
+            } else if (c == '[') {
+                // push k into count
+                count.push(k);
+                k = 0;
+                // give a stringbuilder as place holder? until reach the ']', then start
+                // building content?
+                // push previous string builder? but with an empty one when first or only one
+                // no, the current stringbuilder won't dissappear
+                //don't think thi is correct
+            } else if (c == ']') {
+                // start building string
+                StringBuilder decode = sb;
+                for (int popk = count.pop(); popk > 0; popk--) {
+                    decode.append(sb);
+                }
+                sb = decode;
+            } else {
+                // recording content
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
     }
 
     public static void main(String[] args) throws Exception {
