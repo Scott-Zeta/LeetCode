32e4f6b37768f4aa71027b0580b6e981f4c90a0c
Scott-Zeta <wc070211@gmail.com>
2022-10-19 11:03:40 +1030

    day14 ex2

---
+++ Level_1/Day_14_Tree/653. Two Sum IV - Input is a BST/src/App.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class App {
+    public boolean findTarget(TreeNode root, int k) {
+        // since binary tree is inorder, can use list two pointer approach
+        List<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        int left = 0;
+        int right = list.size() - 1;
+        while (left < right) {
+            int sum = list.get(left) + list.get(right);
+            if (sum == k) {
+                return true;
+            } else if (sum > k) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return false;
+    }
+
+    public void inOrder(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left, list);
+        list.add(node.val);
+        inOrder(node.right, list);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
