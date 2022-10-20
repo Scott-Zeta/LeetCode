62adda60629e3c5890b9b66ae9528874f2d1925e
Scott-Zeta <wc070211@gmail.com>
2022-10-20 13:27:33 +1030

    day2 ex1

--- Level_1/Day_2/35. Search Insert Position/src/App.java
+++ Level_1/Day_2/35. Search Insert Position/src/App.java
@@ -1,8 +1,26 @@
 public class App {
     public int searchInsert(int[] nums, int target) {
-        
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = left + (right - left);
+
+        while (left <= right) {
+            mid = left + (right - left);
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (target > nums[mid]) {
+            return mid + 1;
+        }
+        return mid;
     }
-    
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
