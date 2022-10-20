6ff66346f9a43f04d9a0c46902f839bf3a2ef052
Scott-Zeta <wc070211@gmail.com>
2022-10-20 12:28:45 +1030

    move directory

---
+++ Level_1/Day_2/35. Search Insert Position/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public int searchInsert(int[] nums, int target) {
+        
+    }
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
