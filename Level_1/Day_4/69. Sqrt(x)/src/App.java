d44e8ddf9244c670e45c9caff8c6128cd7ae42e2
Scott-Zeta <wc070211@gmail.com>
2022-10-22 12:16:29 +1030

    sqrt

---
+++ Level_1/Day_4/69. Sqrt(x)/src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static int mySqrt(int x) {
+        if(x == 1){
+            return 1;
+        }
+        int left = 0;
+        int right = x;
+        while (right >= left) {
+            int mid = left + (right - left) / 2;
+            if (mid == x / mid) {
+                return mid;
+            }
+            if (mid < x / mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+
+    public static void main(String[] args) throws Exception {
+        for(int i = 1; i < 17; i++){
+            System.out.println(i + ": " + mySqrt(i));
+        }
+        // System.out.println(mySqrt(1));
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Level_1/Day_4/744. Find Smallest Letter Greater Than Target/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public char nextGreatestLetter(char[] letters, char target) {
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
