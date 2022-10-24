87417962efaee34193b382916de444a34c8f9e71
Scott-Zeta <wc070211@gmail.com>
2022-10-24 19:51:45 +1030

    Is this too easy?

---
+++ Level_1/Day_6/441. Arranging Coins/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static int arrangeCoins(int n) {
+        int count = 1;
+        while (n > 0) {
+            n = n - count;
+            count++;
+        }
+        return count - 2;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(arrangeCoins(11));
+        System.out.println("Hello, World!");
+    }
+}
