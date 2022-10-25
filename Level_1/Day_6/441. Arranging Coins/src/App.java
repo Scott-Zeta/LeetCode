8082d51043619e342110a7bc309afd48cba0fd3b
Scott-Zeta <wc070211@gmail.com>
2022-10-25 14:25:48 +1030

    add binary method

--- Level_1/Day_6/441. Arranging Coins/src/App.java
+++ Level_1/Day_6/441. Arranging Coins/src/App.java
@@ -8,6 +8,24 @@ public class App {
         return count - 2;
     }
 
+    public int binaryArrangeCoins(int n) {
+        long left = 0, right = n;
+        long k, curr;
+        while (left <= right) {
+          k = left + (right - left) / 2;
+          curr = k * (k + 1) / 2;
+    
+          if (curr == n) return (int)k;
+    
+          if (n < curr) {
+            right = k - 1;
+          } else {
+            left = k + 1;
+          }
+        }
+        return (int)right;
+      }
+
     public static void main(String[] args) throws Exception {
         System.out.println(arrangeCoins(11));
         System.out.println("Hello, World!");
