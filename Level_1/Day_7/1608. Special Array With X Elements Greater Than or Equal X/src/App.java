226ceffa4921d3597fbf534a1fa73c7c05cea019
Scott-Zeta <wc070211@gmail.com>
2022-10-28 16:59:33 +1030

    day7 ex2

--- Level_1/Day_7/1608. Special Array With X Elements Greater Than or Equal X/src/App.java
+++ Level_1/Day_7/1608. Special Array With X Elements Greater Than or Equal X/src/App.java
@@ -1,6 +1,24 @@
 public class App {
     public int specialArray(int[] nums) {
-        
+        int left = 0;
+        int right = nums.length;
+        while (right >= left) {
+            int mid = left + (right - left) / 2;
+            int count = 0;
+            for (int num : nums) {
+                if (num >= mid) {
+                    count++;
+                }
+            }
+            if (count > mid) {
+                left = mid + 1;
+            } else if (count < mid) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) throws Exception {
