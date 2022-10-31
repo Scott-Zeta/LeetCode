70c2252b9f4474c9d9f9d4e5b89fb7dbd84f08ad
Scott-Zeta <wc070211@gmail.com>
2022-10-31 16:12:19 +1030

    add HashSet method

--- Level_1/Day_9/1346. Check If N and Its Double Exist/src/App.java
+++ Level_1/Day_9/1346. Check If N and Its Double Exist/src/App.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class App {
     public boolean checkIfExist(int[] arr) {
@@ -12,6 +13,21 @@ public class App {
                 // prevent it is zero itself at same position
                 return true;
             }
+        }
+        return false;
+    }
+
+    public boolean HashSetcheckIfExist(int[] arr) {
+        //faster since set.contains O(logn)
+        //Hashmap.contains O(n)
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int n : arr) {
+            if (set.contains(n * 2) || (n % 2 == 0 && set.contains(n / 2))) {
+                //contains its double, or its half when it is even number
+                return true;
+            }
+            set.add(n);
         }
         return false;
     }
