142189db1e89cc6c39ec1b494a37e639bd385e06
Scott-Zeta <wc070211@gmail.com>
2022-10-31 15:35:44 +1030

    add binary method

--- Level_1/Day_9/1337. The K Weakest Rows in a Matrix/src/App.java
+++ Level_1/Day_9/1337. The K Weakest Rows in a Matrix/src/App.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class App {
-    public int[] kWeakestRows(int[][] mat, int k) {
+    public int[] LinearkWeakestRows(int[][] mat, int k) {
         // linear solution
         int m = mat.length;
         int n = mat[0].length;
@@ -47,6 +48,52 @@ public class App {
                 break;
             }
         }
+        return result;
+    }
+
+    private int binarySearch(int[] row) {
+        int low = 0;
+        int high = row.length;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (row[mid] == 1) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    public int[] kWeakestRows(int[][] mat, int k) {
+        // binary and piroruity queue method
+        int m = mat.length;
+        int n = mat[0].length;
+
+        // build pq with compartor first on strength then on index
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            if (a[0] == b[0])
+                return b[1] - a[1];
+            else
+                return b[0] - a[0];
+        });
+
+        for (int i = 0; i < m; i++) {
+            // use binary search to confirm the last 1 as strength
+            int strength = binarySearch(mat[i]);
+            // build a strength and index tupple for pq
+            int[] entry = new int[] { strength, i };
+            pq.add(entry);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = pq.poll()[1];
+        }
+
         return result;
     }
 
