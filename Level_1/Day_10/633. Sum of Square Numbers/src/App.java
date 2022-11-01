fa752f44035efeb454d78767fd70410e63e70d89
Scott-Zeta <wc070211@gmail.com>
2022-11-01 12:06:22 +1030

    day10 ex1

--- Level_1/Day_10/633. Sum of Square Numbers/src/App.java
+++ Level_1/Day_10/633. Sum of Square Numbers/src/App.java
@@ -1,11 +1,11 @@
 public class App {
     public static boolean judgeSquareSum(int c) {
-        int mid = (int) Math.sqrt(c / 2);
-        int left = mid;
-        int right = mid;
+        long mid = (long) Math.sqrt(c / 2);
+        long left = mid;
+        long right = mid;
 
         while (left >= 0) {
-            int sum = left * left + right * right;
+            long sum = left * left + right * right;
             if (sum > c) {
                 left--;
             } else if (sum < c) {
@@ -20,7 +20,7 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println(judgeSquareSum(3));
+        System.out.println(judgeSquareSum(85));
         System.out.println("Hello, World!");
     }
 }
