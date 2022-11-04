aa83d482a04fd0528d1a9ca87e1f79bc5afb4cd1
Scott-Zeta <wc070211@gmail.com>
2022-11-05 10:12:02 +1030

    add brute force

--- Level_1/Day_2_Two_Pointers/189. Rotate Array/src/App.java
+++ Level_1/Day_2_Two_Pointers/189. Rotate Array/src/App.java
@@ -1,23 +1,28 @@
 public class App {
-    public static void rotate(int[] nums, int k) {
-        int n = nums.length;
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-            result[i] = nums[k];
-            k++;
-            if (k >= n) {
-                k = 0;
+    public static void BruteRotate(int[] nums, int k) {
+        // the question does not mean get the result
+        // but recur the process of rotation
+        // also need to consider if k > nums.length(rotated more than one cycle)
+        k %= nums.length;
+
+        int temp, previous;
+        for (int i = 0; i < k; i++) {
+            previous = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                temp = nums[j];
+                nums[j] = previous;
+                previous = temp;
             }
-        }
-        nums = result;
-        for(int i = 0; i<n; i++){
-            System.out.println(nums[i]);
         }
     }
 
+    public static void rotate(int[] nums, int k){
+        
+    }
+
     public static void main(String[] args) throws Exception {
-        int[] num = {0,1,2,3,4,5};
-        rotate(num,2);
+        int[] num = { 1, 2, 3, 4, 5, 6, 7 };
+        rotate(num, 3);
         System.out.println("Hello, World!");
     }
 }
