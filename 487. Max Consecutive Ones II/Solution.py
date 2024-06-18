201e8d43f2b355cfd765801ad27fff99eaf19f14
Scott Zeta <scott-zeta@outlook.com>
2024-06-18 14:01:05 +0930

    487(my solution, not ideal)

---
+++ 487. Max Consecutive Ones II/Solution.py
@@ -0,0 +1,52 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 1:
+            return 1
+        count = 0
+        sum_arraies = []
+        sub_sum_array = []
+        for i in range(len(nums)):
+            if i == len(nums) - 1:
+                if nums[i] == 0:
+                   sub_sum_array.append(count)
+                   count = 0
+                   sub_sum_array.append(count)
+                   sum_arraies.append(sub_sum_array)
+                if nums[i] == 1:
+                    count += 1
+                    sub_sum_array.append(count)
+                    sum_arraies.append(sub_sum_array)
+            else:        
+                if nums[i] == 1:
+                    count += 1
+                if nums[i] == 0:
+                    sub_sum_array.append(count)
+                    count = 0
+                    if nums[i+1] == 0:
+                        sub_sum_array.append(count)
+                        sum_arraies.append(sub_sum_array)
+                        sub_sum_array = []
+        #print(sum_arraies)
+        
+        max = 0
+        for array in sum_arraies:
+            if len(array) == 1:
+                return array[0]
+            for i in range(len(array) - 1):
+                consecutive = array[i] + array[i+1] + 1
+                if consecutive > max:
+                    max = consecutive
+        return max
+        
+        
+test = Solution()
+print(test.findMaxConsecutiveOnes([1,0,1,1,0]))
+print(test.findMaxConsecutiveOnes([1,0,1,1,0,1,1]))
+print(test.findMaxConsecutiveOnes([0,0,1,0,1,1,0,1,0,0,0,1,0]))
+print(test.findMaxConsecutiveOnes([1,1,1]))
+print(test.findMaxConsecutiveOnes([0,0,0]))
+print(test.findMaxConsecutiveOnes([1,0,1,1,0,1]))            
