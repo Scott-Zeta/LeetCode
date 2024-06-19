705fedf74df116add098deb0c35fa7436420609d
Scott Zeta <scott-zeta@outlook.com>
2024-06-19 15:39:28 +0930

    414 add heap

--- 414. Third Maximum Number/Solution.py
+++ 414. Third Maximum Number/Solution.py
@@ -15,3 +15,23 @@ class Solution(object):
             nums_set.remove(max_num)
         
         return max(nums_set)
+    
+    def heap_thirdMax(self,nums):
+        import heapq
+        nums_set = set(nums)
+        min_heap = []
+        for num in nums_set:
+            if len(min_heap) >= 3:
+                if num > min_heap[0]:
+                    heapq.heappush(min_heap,num)
+                    heapq.heappop(min_heap)
+            else:
+                heapq.heappush(min_heap,num)
+        print(min_heap)
+        if len(min_heap) < 3:
+            return min_heap[-1]
+        
+        return min_heap[0]
+
+test = Solution()
+print(test.thirdMax([1,2,2,5,3,5]))
